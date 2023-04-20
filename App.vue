52912f516fbf9e3cb481876626ca24576830a29c
FelixOnyangoh1 <felix.onyangoh1@gmail.com>
2023-04-20 15:45:23 +0300

    third commit

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>API Data</h1>
+    <api-consumer></api-consumer> <!-- use the ApiConsumer component here -->
+  </div>
+</template>
+
+<script>
+import ApiConsumer from './ApiConsumer.vue'; // import the ApiConsumer component
+
+export default {
+  components: {
+    ApiConsumer, // register the ApiConsumer component
+  },
+};
+</script>
+
